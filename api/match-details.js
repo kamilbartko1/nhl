9c1f085facfe7af72a3800c5d9501e80d6cbdb21
Kamil <kamil.bartko1@gmail.com>
2025-10-08 18:48:48 +0200

    snad to uz bude fungovat

--- api/match-details.js
+++ api/match-details.js
@@ -1,21 +1,21 @@
+// dopisem si tu nieco, aby som poznacil svoj kod
+// api/match-details.js
 import axios from "axios";
 
 const API_KEY = "WaNt9YL5305o4hT2iGrsnoxUhegUG0St1ZYcs11g";
 
 export default async function handler(req, res) {
   try {
-    const { homeId, awayId } = req.query; // teraz parametre idú cez query string
-    const url = `https://api.sportradar.com/icehockey/trial/v2/en/competitors/${homeId}/versus/${awayId}/summaries.json?api_key=${API_KEY}`;
-    const response = await axios.get(url);
-
-    const lastMeeting = response.data.last_meetings?.[0];
-    if (!lastMeeting) {
-      return res.status(404).json({ error: "Žiadny zápas nenájdený" });
+    const { gameId } = req.query;
+    if (!gameId) {
+      return res.status(400).json({ error: "Chýba gameId parameter" });
     }
 
-    res.status(200).json(lastMeeting);
+    const url = `https://api.sportradar.com/nhl/trial/v7/en/games/${gameId}/boxscore.json?api_key=${API_KEY}`;
+    const response = await axios.get(url);
+    res.status(200).json(response.data);
   } catch (err) {
-    console.error(err.message);
+    console.error("Chyba pri načítaní detailov zápasu:", err.message);
     res.status(500).json({ error: "Chyba pri načítaní detailov zápasu" });
   }
 }
